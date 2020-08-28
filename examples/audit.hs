f249005abd9b7d6368f509a8cccabfbdc91616ab
Magicloud <1886157+Magicloud@users.noreply.github.com>
2020-08-28 22:18:44 +0800

    Using Optics in example/audit

--- examples/audit.hs
+++ examples/audit.hs
@@ -1,22 +1,33 @@
-{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 import Data.Either
 import Data.HCL
 import Data.HCL.Types
+import Data.Maybe
 import Data.Text.IO as T
+
+import Optics.Core
+
 import System.Environment
+
 import Text.Megaparsec
+import Text.Pretty.Simple ( pPrint )
 
 main :: IO ()
-main = do
-  home <- getEnv "HOME"
-  T.readFile (home ++ "/src/Work/terraform-aws-foundation/modules/vpc-scenario-2/variables.tf") >>= print . filter (not . varWithDesc) . fromRight [] . runParser (hclDoc <* eof) "variables.tf"
+main =
+  do home <- getEnv "HOME"
+     T.readFile (home
+                 ++ "/src/Work/terraform-aws-foundation/modules/vpc-scenario-2/variables.tf")
+       >>= pPrint
+       . filter (not . varWithDesc)
+       . fromRight []
+       . runParser (hclDoc <* eof) "variables.tf"
 
 varWithDesc :: Value -> Bool
-varWithDesc (VObject _ _ (PT _ _ "variable" : _) ps _) =
-  case filter (\case
-                VPair _ _ (PT _ _ "description") _ -> True
-                _ -> False) ps of
-    [] -> False
-    _ -> True
-varWithDesc _ = False
+varWithDesc v =
+  isJust (preview _VObject v)
+  && preview (voIdents % ix 0 % text) v == Just "variable"
+  && anyOf (voAssignments % each)
+           ((==) (Just "description") . preview (vpKey % text))
+           v
