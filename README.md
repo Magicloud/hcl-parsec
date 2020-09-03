986c64820ed3f10822a1aa271923b9239e0596a0
Magicloud <1886157+Magicloud@users.noreply.github.com>
2020-09-03 17:13:40 +0800

    Revert "Using Optics in example/audit"
    This reverts commit f249005abd9b7d6368f509a8cccabfbdc91616ab.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This library aims to provide the abilities of processing HCL (Hashicorp Configuration Language) is Haskell. Existing `language-hcl` hackage does not seem to work any more.
 
-Right now, the library supports parsing latest syntax. And Optics. Planning to rework the `Value` data structure to make the Optics usage more reasonable.
+Right now, the library supports parsing latest syntax. And I am still wondering if Lens can be introduced to ease the accessing to the content within the parsing result.
 
 `examples` folder provides two samples of using the library. They should not be considered strict/safe or to use as serious tools.
 
--- src/Data/HCL/Types.hs
+++ src/Data/HCL/Types.hs
@@ -1,79 +1,42 @@
 {-# LANGUAGE TemplateHaskell #-}
-
 module Data.HCL.Types where
 
-import Data.Map ( Map )
-import Data.Scientific ( Scientific )
-import Data.Text ( Text )
-import Data.Void ( Void )
-
+import Data.Map (Map)
+import Data.Scientific (Scientific)
+import Data.Text (Text)
+import Data.Void (Void)
 import Optics.Lens
 import Optics.Prism
 import Optics.TH
-
-import Text.Megaparsec ( Parsec )
-import Text.Megaparsec.Pos ( SourcePos )
+import Text.Megaparsec (Parsec)
+import Text.Megaparsec.Pos (SourcePos)
 
 type Parser = Parsec Void Text
 
-data PosedText =
-  PT { _ptFrom :: SourcePos
-     , _ptTo :: SourcePos
-     , _text :: Text }
-  deriving ( Eq, Ord, Show )
-
+data PosedText = PT { _from :: SourcePos
+                    , _to :: SourcePos
+                    , _text :: Text }
+               deriving (Eq, Ord, Show)
 makeFieldLabels ''PosedText
-
 makeLenses ''PosedText
 
-data StringPart
-  = SPPlain PosedText
-  | SPInterp PosedText
-  deriving ( Eq, Ord, Show )
-
+data StringPart = SPPlain PosedText
+                | SPInterp PosedText
+                deriving (Eq, Ord, Show)
 makePrisms ''StringPart
 
-type Doc = [ Value ]
+type Doc = [Value]
 
-data Value
-  = VNumber { _vnFrom :: SourcePos
-            , _vnTo :: SourcePos
-            , _vn :: Scientific }
-  | VString { _vsFrom :: SourcePos
-            , _vsTo :: SourcePos
-            , _vsString :: [ StringPart ] }
-  | VBool { _vbFrom :: SourcePos
-          , _vbTo :: SourcePos
-          , _vbBool :: Bool }
-  | VIdents { _viFrom :: SourcePos
-            , _viTo :: SourcePos
-            , _vi :: [ PosedText ] }
-  | VObject { _voFrom :: SourcePos
-            , _voTo :: SourcePos
-            , _voIdents :: [ PosedText ]
-            , _voAssignments :: [ Value ]
-            , _voNestedObjects :: [ Value ] }
-  | VMap { _vmFrom :: SourcePos
-         , _vmTo :: SourcePos
-         , _vmMap :: Map PosedText Value }
-  | VList { _vlFrom :: SourcePos
-          , _vlTo :: SourcePos
-          , _vlList :: [ Value ] }
-  | VFunction { _vfFrom :: SourcePos
-              , _vfTo :: SourcePos
-              , _vfName :: PosedText
-              , _vfParameters :: [ Value ] }
-  | VPair { _vpFrom :: SourcePos
-          , _vpTo :: SourcePos
-          , _vpKey :: PosedText
-          , _vpValue :: Value }
-  | VIndexing { _viFrom :: SourcePos
-              , _viTo :: SourcePos
-              , _viList :: Value
-              , _viIndex :: Value }
-  | VUnit
-  deriving ( Eq, Ord, Show )
-
+data Value = VNumber SourcePos SourcePos Scientific
+           | VString SourcePos SourcePos [StringPart]
+           | VBool SourcePos SourcePos Bool
+           | VIdents SourcePos SourcePos [PosedText]
+           | VObject SourcePos SourcePos [PosedText] [Value] [Value] -- assignment object
+           | VMap SourcePos SourcePos (Map PosedText Value)
+           | VList SourcePos SourcePos [Value]
+           | VFunction SourcePos SourcePos PosedText [Value]
+           | VPair SourcePos SourcePos PosedText Value
+           | VIndexing SourcePos SourcePos Value Value -- array index
+           | VUnit
+           deriving (Eq, Ord, Show)
 makePrisms ''Value
-
-makeLenses ''Value
